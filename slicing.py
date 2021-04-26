802fa0ffef1733968c0c59e11b705867725fe263
saduqr08 <79064129+saduqr08@users.noreply.github.com>
2021-04-25 18:18:59 -0700

    1st commit to development branch

--- slicing.py
+++ slicing.py
@@ -1,18 +1,43 @@
+data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+
 def exchange_first_last(seq):
-    pass
+    first = seq[-1:]
+    mid = seq[1:-1]
+    last = seq[:1]
+    return print(first + mid + last)
+
+
+exchange_first_last(data)
 
 
 def every_other_removed(seq):
-    pass
+    return print(seq[::2])
+
+
+every_other_removed(data)
 
 
 def first4_last4_every_other_removed(seq):
-    pass
+
+    return print(seq[1:-1:2])
+
+
+first4_last4_every_other_removed(data)
 
 
 def seq_reversed(seq):
-    pass
+    return print(seq[::-1])
+
+
+seq_reversed(data)
 
 
 def last_third_first_third_mid_third(seq):
-    pass
+    first = seq[-4:]
+    mid = seq[6:10]
+    last = seq[:4]
+    return print(first + last + mid)
+
+
+last_third_first_third_mid_third(data)
