bba7cd98ff22594f9e7325800d3dc6a294b8f158
saduqr08 <79064129+saduqr08@users.noreply.github.com>
2021-04-25 18:15:23 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# py210-template-slicing-lab
+
+
+[Assignment Details](https://uwpce-pythoncert.github.io/ProgrammingInPython/exercises/slicing.html)
---
+++ slicing.py
@@ -0,0 +1,18 @@
+def exchange_first_last(seq):
+    pass
+
+
+def every_other_removed(seq):
+    pass
+
+
+def first4_last4_every_other_removed(seq):
+    pass
+
+
+def seq_reversed(seq):
+    pass
+
+
+def last_third_first_third_mid_third(seq):
+    pass
